6b3fc37bcc635c74f3777b8b17fe4cb62268d17b
Stackers <stasix9099@gmail.com>
2026-02-12 19:28:52 +0500

    Created a new Report structure and adjusted previous structures

--- Lysov_Stas/Project.cpp
+++ Lysov_Stas/Project.cpp
@@ -1,14 +1,14 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <filesystem>
+#include <vector>
 using namespace std;
 
 // typedef int RUB;  // C
 using RUB = long long int;  // C++
 using BUB = long double;
-
 
 struct Expenses {
     string name;
@@ -23,9 +23,10 @@ struct Income {
 };
 
 struct Wallet {
-    Income income;
-    Expenses expenses;
+    vector<Income> income;
+    vector<Expenses> expenses;
     BUB balance;
+	BUB salary;
 };
 
 struct Car {
@@ -42,93 +43,103 @@ struct Person alice;
 
 void alice_init()
 {
-    alice.capital = 0;
     alice.wallet.balance = 180'000;
+	alice.wallet.salary = 150'000;
 
-    alice.car.value = 15'400'000;
-    alice.house_value = 40'000'000;
+	alice.car.value = 2'000'000;
+    alice.house_value = 4'000'000;
+
+    alice.capital = alice.wallet.balance + alice.car.value + alice.house_value;
 }
 
 void expenses_init(string name, RUB value, string date)
 {
-    alice.wallet.expenses.name = name;
-    alice.wallet.expenses.value = value;
-    alice.wallet.expenses.date = date;
-    cout << "Expense added: " << alice.wallet.expenses.name << ", " << alice.wallet.expenses.value << ", " << alice.wallet.expenses.date << endl;
+    Expenses exp{ name, value, date };
+    alice.wallet.expenses.push_back(exp);
+    alice.wallet.balance -= value;
+
+    cout << "Expense added!\n";
 }
 
 void income_init(string name, RUB value, string date)
 {
-    alice.wallet.income.name = name;
-    alice.wallet.income.value = value;
-    alice.wallet.income.date = date;
-    cout << "Income added: " << alice.wallet.income.name << ", " << alice.wallet.income.value << ", " << alice.wallet.income.date << endl;
+    Income inc{ name, value, date };
+    alice.wallet.income.push_back(inc);
+    alice.wallet.balance += value;
+    cout << "Income added!\n";
 }
 
-void balance_init()
+void alice_salary()
 {
-    alice.wallet.balance = alice.wallet.income.value - alice.wallet.expenses.value;
-    cout << "Balance updated: " << alice.wallet.balance << endl;
+    alice.wallet.balance += alice.wallet.salary;
 }
 
-void mains(int sizexp)
-{
-    int *exp = new int[sizexp];
-}
-// float sberbank_interest(RUB sum)   // Сделать sum 
-// {
-//     if (sum > 10'000 and sum < 3'000'000) {
-//         return 11.5;
-//     }
+float sberbank_interest(RUB sum)   // Сделать sum 
+ {
+     if (sum > 10'000 and sum < 3'000'000) {
+         return 11.5;
+     }
 
-//     if (sum >= 3'000'000) {
-//         return 12.5;  //  125 -1 +
-//     }
+     if (sum >= 3'000'000) {
+         return 12.5;  //  125 -1 +
+     }
 
-//     return 0.1;
-// }
+     return 0.1;
+ }
 
+ void alice_bank_interest()
+ {
+     float pp;
+     pp = sberbank_interest(alice.capital);
+     alice.capital += alice.capital * (pp / 12.0 / 100.0);
+ }
 
+ void alice_simulation()
+ {
+     int year = 2026;
+     int month = 2;
 
-// void alice_bank_interest()
-// {
-//     float pp;
-//     pp = sberbank_interest(alice.capital);
-//     alice.capital += alice.capital * (pp / 12.0 / 100.0);
-// }
+     while ( not (year == 2027 and month == 2) ) {
+         // my_cat();
+         // my_trip();
+         alice_bank_interest();
+         alice_salary();
 
+         ++month;
+         if (month == 13) {
+             ++year;
+             month = 1;
+         }
+     }
+ }
 
-// void alice_simulation()
-// {
-//     int year = 2026;
-//     int month = 2;
+ void financial_report()
+ {
+     cout << "\n_________ FINANCIAL REPORT _________\n";
+     cout << "Wallet balance: " << alice.wallet.balance << endl;
+     cout << "Bank capital: " << alice.capital << endl;
+     cout << "Car value: " << alice.car.value << endl;
+     cout << "House value: " << alice.house_value << endl;
 
-//     while ( not (year == 2027 and month == 2) ) {
-//         // my_cat();
-//         // my_trip();
-//         alice_food();
-//         alice_car();
-//         alice_bank_interest();
-//         alice_salary(year, month);
-
-//         ++month;
-//         if (month == 13) {
-//             ++year;
-//             month = 1;
-//         }
-//     }
-// }
+     cout << "Total net worth: "
+         << alice.wallet.balance
+         + alice.capital
+         + alice.car.value
+         + alice.house_value
+         << endl;
+ }
 
 
 int main()
 {
+    alice_init();
     int sizexp = 0;
     cout << "Capital: " << alice.wallet.balance + alice.car.value + alice.house_value << endl;
 
     while (true)
     {
         string operations;
-        cout << "Enter operations (Add_expense, Add_income, Check_balance): ";
+        cout << "Enter operations (Add_expense, Add_income, Check_balance, Report): ";
         cin >> operations;
 
         if (operations == "Add_expense") {
@@ -143,8 +154,9 @@ int main()
             string date_expense;
             cout << "Date of expense: ";
             cin >> date_expense;
-            sizexp++;
-            mains(sizexp);
+
+           
+         
 
             expenses_init(name_expense, value_expense, date_expense);
             cout << "Expense added! ";
@@ -167,8 +179,14 @@ int main()
             cout << "Income added! ";
         }
         else if (operations == "Check_balance") {
-            balance_init();
+            cout << "Current balance: "
+                << alice.wallet.balance
+                << endl;
+		}
+        else if (operations == "Report") {
+            financial_report();
         }
+        
         else {
             cout << "Invalid operation!" << endl;
         }
