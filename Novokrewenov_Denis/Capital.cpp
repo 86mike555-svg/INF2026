3dba1573cd095c85848895e7d8f74652e57b8370
Rebus124 <160196048+Rebus124@users.noreply.github.com>
2026-02-12 15:13:53 +0300

    Update Capital.cpp

--- Novokrewenov_Denis/Capital.cpp
+++ Novokrewenov_Denis/Capital.cpp
@@ -1,33 +1,103 @@
 #include <stdio.h>
 
-int salary = 180'001;
-int capital = 0;
+using RUB = long int;
 
-void my_salary(const int month, const int year) {
-    if ((month == 8) && (year == 2026)) {
-            salary *= 1.5;
-    }
+struct Car {
+    RUB value;
+    RUB gas;
+};
 
-    capital += salary;
+struct Cat {
+    RUB feed;
+};
+
+struct Person {
+    RUB salary;
+    RUB capital;
+    RUB food;
+    Car car;
+    Cat cat;
+};
+
+struct Person Alice;
+
+const float pp = 14.5;
+const float cash_inflation_per_month = 1.0125; //15%
+const float salary_inflation_per_month = 1.01; //12%
+
+void alice_cat() {
+    Alice.capital -= Alice.cat.feed;
+}
+void alice_car() {
+    Alice.capital -= Alice.car.gas;
 }
 
-int main() {
-    float pp = 14.5;
+void alice_salary(const int month, const int year) {
+    if ((month == 8) && (year == 2026)) { // Promotion
+        Alice.salary = Alice.salary * 1.5;
+    }
+    Alice.capital += Alice.salary;  
+}
 
+void alice_food() {
+    Alice.capital -= Alice.food;
+}
+
+void alice_bank() {
+    Alice.capital += Alice.capital * (pp / 12.0 / 100.0);
+}
+
+void alice_init() {
+    Alice.capital = 0;
+    Alice.salary = 180000;
+    Alice.food = 20000;
+
+    Alice.car.value = 1000000;
+    Alice.car.gas = 15000;
+
+    Alice.cat.feed = 2000;
+}
+
+void inflation() {
+    Alice.car.value = static_cast<int> (Alice.car.value * cash_inflation_per_month);
+    Alice.car.gas = static_cast<int> (Alice.car.gas * cash_inflation_per_month);
+    Alice.cat.feed = static_cast<int> (Alice.cat.feed * cash_inflation_per_month);
+    Alice.food = static_cast<int> (Alice.food * cash_inflation_per_month);
+    Alice.salary = static_cast<int> (Alice.salary * salary_inflation_per_month);
+}
+
+void simulation() {
     int month = 2;
     int year = 2026;
 
-    while(!((month == 3) && (year == 2027))) {
-        capital += capital*pp/100.0/12.0;
-
-        my_salary(month, year);        
-        
-        if (month == 13){
-            month = 1;
-            ++year;
-        }
+    while (!((month == 3) && (year == 2027))) {
+        alice_cat();
+        alice_food();
+        alice_car();
+        alice_bank();
+        alice_salary(month, year);
+        inflation();
         ++month;
+        if (month == 13) {
+        month = 1;
+        ++year;
+        }
     }
-    printf("Salary = %d\n", salary);
-    printf("Capital = %d", capital);
+}
+
+void print_results() {
+    printf("Capital = %ld\n", Alice.capital);
+    printf("Salary = %ld\n", Alice.salary);
+    printf("Feed = %ld\n", Alice.cat.feed);
+    printf("Food = %ld\n", Alice.food);
+    printf("Car value = %ld\n", Alice.car.value);
+    printf("Car gas = %ld", Alice.car.gas);
+}
+
+int main() { 
+    alice_init();
+
+    simulation();
+  
+    print_results();
 }
