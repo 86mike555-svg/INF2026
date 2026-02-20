a99b6158187e103274d95cbf21d0a6c568174804
lizaGoose <113460041+lizaGoose@users.noreply.github.com>
2026-02-20 12:07:50 +0300

    Practice4 hello_world.c

--- Guseva_Elizaveta/hello_world.c
+++ Guseva_Elizaveta/hello_world.c
@@ -10,6 +10,7 @@ struct Car
     RUB value;
     RUB gas;
 };
+
 struct Bank
 {
     RUB account_rub;
@@ -21,15 +22,18 @@ struct Person
 {
     Bank vtb;
     RUB food;
-    RUB salary;
+    RUB salary_rub;
+    USD salary_usd;
     Car car;
+    RUB lunch_cost;
 };
 struct Person alice;
 
-void food_cost(const int month, const int year)
+void alice_food_cost(const int month, const int year)
 {
     if (month == 12)
         alice.vtb.account_rub -= 2000; // party
+
     Percent inflation = 12.0;
     switch (year)
     {
@@ -47,44 +51,42 @@ void food_cost(const int month, const int year)
         break;
     }
 
-    alice.food += alice.food * (inflation / 100. / 12.);
+    alice.food += (alice.food + alice.lunch_cost * 247) * (inflation / 100. / 12.); // 247 - average number of working days in a month
 
     alice.vtb.account_rub -= alice.food;
 }
 
-void print_results()
+void count_capital()
 {
-    printf("Salary = %lld\n", alice.salary);
     RUB capital = 0;
     capital += alice.vtb.account_rub;
     capital += alice.car.value;
     capital += alice.vtb.account_usd * alice.vtb.rate_usd_rub;
-    printf("Capital = %lld", alice.vtb.account_rub);
 }
 
-void alice_salary(int month, int year)
+void print_results()
+{
+    printf("Capital_usd = %lld", alice.vtb.account_usd);
+    printf("Capital_rub = %lld", alice.vtb.account_rub);
+}
+
+void alice_first_job(const int month, const int year)
 {
     if (month == 2 && year == 2026)
-    {
-        alice.vtb.account_rub += 5000;
-    }
+        alice.vtb.account_rub += 5000; // bonus
 
     if (month == 3)
-    {
-        alice.salary *= 1.5;
-    }
+        alice.salary_rub *= 1.5; // promotion
 
-    alice.vtb.account_rub += alice.salary; // comment
+    alice.vtb.account_rub += alice.salary_rub;
 }
 
-void second_job(const int bonus, const int lunch_cost, const int bonus_month)
+void alice_second_job(const int month, const int year)
 {
-    alice.vtb.account_usd += 1000;
-    if (bonus_month == 5)
-    {
-        alice.vtb.account_usd += bonus;
-    }
-    alice.vtb.account_rub -= lunch_cost * 30;
+    if (month == 5 && year == 2027)
+        alice.vtb.account_usd += 3000; // bonus
+
+    alice.vtb.account_usd += alice.salary_usd;
 }
 
 void alice_car()
@@ -92,30 +94,48 @@ void alice_car()
     alice.vtb.account_rub -= alice.car.gas;
 }
 
+void alice_trip()
+{
+    if (alice.vtb.account_usd > 6000)
+        alice.vtb.account_usd -= 6000; // trip to Maldives
+}
+
+void alice_cat(const int month, const int year)
+{
+    if (month == 12 && year == 2027)
+        alice.vtb.account_usd -= 1000; // start-up costs
+
+    if (month == 1)
+        alice.vtb.account_rub -= 3000; // veterinary costs
+
+    alice.vtb.account_rub -= 2000; // food
+    alice.vtb.account_rub -= 500;  // litter
+}
+
 void simulation()
 {
     int year = 2026;
     int month = 2;
 
-    while (!(year == 2027 && month == 2))
+    while (!(year == 2028 && month == 2))
     {
-        food_cost(month, year);
-
-        ++month;
         if (month == 13)
         {
             ++year;
             month = 1;
         }
-        alice_salary(month, year);
-        second_job(3000, 5, month);
+
+        alice_food_cost(month, year);
+        alice_first_job(month, year);
+        alice_second_job(month, year);
         alice_car();
-        // alice_cat();
-        // alice_trip();
+        alice_cat(month, year);
+        alice_trip();
         // alice_rent();
         // alice_mortgage();
+
+        ++month;
     }
-    print_results();
 }
 
 void alice_init()
@@ -125,7 +145,9 @@ void alice_init()
     alice.vtb.account_usd = 10000;
 
     alice.food = 1800;
-    alice.salary = 180000;
+    alice.lunch_cost = 5;
+    alice.salary_rub = 180000;
+    alice.salary_usd = 1500;
 
     alice.car.value = 2400000;
     alice.car.gas = 15000;
@@ -133,6 +155,7 @@ void alice_init()
 
 int main()
 {
+    print_results();
     alice_init();
     simulation();
 }
