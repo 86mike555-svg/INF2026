4f5965678691ec0f1229f7847f51ce5e12db83dc
senya807 <arseny.kanterman@yandex.ru>
2026-02-21 10:24:43 +0300

    Add files via upload

---
+++ Kanterman Arseny/алиса.cpp
@@ -0,0 +1,135 @@
+#include <stdio.h>
+using Percent = float;
+using RUB = long long int;
+using USD = long long int;
+using Time = int;
+
+struct Car {
+	RUB value;
+	RUB gas;  
+};
+
+
+struct Bank {
+	RUB account_rub;
+	USD account_usd;
+	float rate_usd_rub;
+};
+struct Credit {
+	RUB debt;
+	Time month;
+	RUB payment;
+	RUB remainder;
+	Percent rate;
+	
+};
+
+struct Person { // Зачем добовлять опять в struct ?
+	Bank vtb;
+	RUB salary;
+	RUB food;
+	Car car;
+	Credit credit;
+};
+
+struct Person alice;
+
+void alice_credit(const int month, const int year) {////
+}
+
+void alice_init() {
+	alice.vtb.rate_usd_rub = 78.76;
+	alice.vtb.account_rub = 0;
+	alice.vtb.account_usd = 1000;
+	alice.salary = 180000;
+	alice.food = 3000;
+	alice.car.value = 24000;
+	alice.car.gas = 15000;
+	alice.credit.debt = 3'000'000;
+	alice.credit.payment= 40'000;
+	alice.credit.month = (alice.credit.debt/ alice.credit.payment);
+}
+
+void total_money() { //скок всего денег
+	RUB total_rub = alice.vtb.account_rub;  
+	RUB dollars_in_rub = alice.vtb.account_usd * alice.vtb.rate_usd_rub;  
+	RUB total = total_rub + dollars_in_rub;  
+	
+	printf("Total money: %lld RUB\n", total);
+	printf("  Rubles: %lld RUB\n", alice.vtb.account_rub);
+	printf("  And dollars: %lld USD = %lld RUB\n", alice.vtb.account_usd, dollars_in_rub);
+}
+
+void yearly_income() { //доход за год
+	RUB yearly = alice.salary * 12;  
+	printf("Year salary: %lld RUB\n", yearly); 
+	printf(" %lld RUB a month\n", alice.salary);
+	printf("  And %lld RUB a day\n", yearly / 365);
+}
+
+
+void alice_food(const int month,const int year)
+{
+	if (month == 12) alice.vtb.account_rub -= 2000;  // christmas party
+	Percent inflation = 12.0;
+	switch (year) {
+		case 2026:inflation = 12.5;break;
+		case 2027:inflation = 14.0;break;
+		case 2028:inflation = 13.0;break;
+		case 2029:inflation = 11.5;break;
+	}
+	alice.food += alice.food * (inflation / 100 / 12);
+	alice.vtb.account_rub -= alice.food;
+}
+
+
+void alice_salary(const int month, const int year) {
+	if (month == 3) {
+		alice.salary *= 1.5;
+	}
+	if (month == 2 && year == 2026) {
+		alice.vtb.account_rub += 5000;  //bonus
+	}
+	alice.vtb.account_rub += alice.salary;
+}
+
+
+void print_results()
+{
+	printf("Salary = %lld\n", alice.salary);
+	
+	RUB capital = 0;
+	capital += alice.vtb.account_rub;
+	capital += alice.vtb.account_usd * alice.vtb.rate_usd_rub;
+	capital += alice.car.value;
+	printf("Capital = %lld \n", capital);
+	total_money();
+	yearly_income();
+}
+
+
+void alice_car()  // const int month, const int year ?
+{
+	alice.vtb.account_rub -= alice.car.gas;
+}
+void simulation() {
+	int year = 2026;
+	int month = 2;
+	while (!(year == 2029 && month == 2)) {
+		alice_salary(month, year);
+		alice_car();
+		alice_food(month, year);
+		++month;
+		if (month == 13) {
+			++year;
+			month = 1;
+		}
+		
+	}
+}
+
+int main() {
+	alice_init();
+	simulation();
+	print_results();
+}
